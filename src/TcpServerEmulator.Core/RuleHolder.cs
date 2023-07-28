6e3009a1ee1bb9d9df3cdf93088d494b08570f01
Geroshabu <oonstks@gmail.com>
2023-07-28 23:06:02 +0900

    RuleHolder で、既存のルールを入れ替えられるようにする

--- src/TcpServerEmulator.Core/RuleHolder.cs
+++ src/TcpServerEmulator.Core/RuleHolder.cs
@@ -12,6 +12,9 @@ namespace TcpServerEmulator.Core
         /// <summary>ルールが削除されたときに発生する</summary>
         public event EventHandler<RuleRemovedEventArgs>? RuleRemoved;
 
+        /// <summary>ルールが入れ替わったときに発生する</summary>
+        public event EventHandler<RuleReplacedEventArgs>? RuleReplaced;
+
         /// <summary>保持しているルール一覧</summary>
         public IEnumerable<IRule> Rules => rules.AsReadOnly();
 
@@ -25,6 +28,25 @@ namespace TcpServerEmulator.Core
         {
             rules.Remove(rule);
             RuleRemoved?.Invoke(this, new RuleRemovedEventArgs(rule));
+        }
+
+        /// <summary>
+        /// コレクション内のルールインスタンスを入れ替える
+        /// </summary>
+        /// <param name="oldRule">入れ替える前の古いルール</param>
+        /// <param name="newRule">入れ替えたい新しいルール</param>
+        /// <exception cref="InvalidOperationException">
+        /// <paramref name="oldRule"/>がコレクションの中に存在しない
+        /// </exception>
+        public void ReplaceRule(IRule oldRule, IRule newRule)
+        {
+            var index = rules.IndexOf(oldRule);
+            if (index == -1)
+            {
+                throw new InvalidOperationException();
+            }
+            rules[index] = newRule;
+            RuleReplaced?.Invoke(this, new RuleReplacedEventArgs(index, oldRule, newRule));
         }
     }
 }
---
+++ src/TcpServerEmulator.Core/RuleReplacedEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using TcpServerEmulator.Rules;
+
+namespace TcpServerEmulator.Core
+{
+    /// <summary>
+    /// コレクション内のルールが新しいルールに入れ替わったときのイベントデータ
+    /// </summary>
+    public class RuleReplacedEventArgs
+    {
+        /// <summary>入れ替わったルールの一覧内での位置</summary>
+        public int Index { get; }
+
+        /// <summary>入れ替わる前のルール</summary>
+        public IRule OldRule { get; }
+
+        /// <summary>入れ替わった後のルール</summary>
+        public IRule NewRule { get; }
+
+        /// <summary>
+        /// <see cref="RuleReplacedEventArgs"/>インスタンスの生成と初期化
+        /// </summary>
+        /// <param name="index">入れ替わったルールの一覧内での位置</param>
+        /// <param name="oldRule">入れ替わる前のルール</param>
+        /// <param name="newRule">入れ替わった後のルール</param>
+        public RuleReplacedEventArgs(int index, IRule oldRule, IRule newRule)
+        {
+            Index = index;
+            OldRule = oldRule;
+            NewRule = newRule;
+        }
+    }
+}
